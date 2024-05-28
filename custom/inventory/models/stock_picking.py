cf9367d48743d38351abac54d0f6901bbb06153e
taknetwendy <taknet_wendy@outlook.com>
2024-05-28 13:47:42 +0800

    Quality V3.22  Changes to be committed: 	modified:   custom/elw_quality/views/quality_alert_view.xml: set widget on stage_id 	modified:   custom/inventory/models/stock_picking.py: add field 'quality_check_count' 	modified:   custom/inventory/views/purchase_order_view.xml: hide 'categ_id' 	modified:   custom/inventory/views/sale_order_view.xml: hide 'categ_id' 	modified:   custom/inventory/views/stock_picking_view.xml: show 'qa_check_product_ids', remove 'Quality Check' button. set color on samrt btns

--- custom/inventory/models/stock_picking.py
+++ custom/inventory/models/stock_picking.py
@@ -43,6 +43,15 @@ class Picking(models.Model):
     quality_check_fail = fields.Boolean(string="Quality Check Fail", compute="_compute_quality_check_fail")
     quality_state = fields.Selection([('none', 'To Do'), ('pass', 'Passed'), ('fail', 'Failed')], )
     quality_check_ids = fields.One2many('elw.quality.check', 'picking_id', string="Quality Status", store=True)
+    quality_check_count = fields.Integer(string="Check Count", compute="_compute_quality_check_count")
+
+    @api.depends('check_ids')
+    def _compute_quality_check_count(self):
+        for rec in self:
+            if rec.check_ids.ids:
+                rec.quality_check_count = sum(1 for check in rec.check_ids)
+            else:
+                rec.quality_check_count = 0
 
     @api.depends('quality_check_ids')
     def _compute_is_all_quality_fails_resolved(self):
