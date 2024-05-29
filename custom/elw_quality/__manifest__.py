b4f7980bce6f53e9078fd4646c34f0305709647a
taknetwendy <taknet_wendy@outlook.com>
2024-05-29 14:27:15 +0800

    V3.303 manifest files: update 'category' as 'category': 'ELW/ELW Inventory',  Changes to be committed: 	modified:   custom/elw_maintenance/__manifest__.py 	modified:   custom/elw_quality/__manifest__.py 	modified:   custom/inventory/__manifest__.py 	modified:   odoo/addons/base/views/ir_module_views.xml: comment newly added 'category_id' for debug purpose display

--- custom/elw_quality/__manifest__.py
+++ custom/elw_quality/__manifest__.py
@@ -16,7 +16,7 @@ Long description of module's purpose
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'ELW',
+    'category': 'ELW/ELW Quality',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -24,6 +24,7 @@ Long description of module's purpose
 
     # always loaded
     'data': [
+        'security/elw_quality_security_data.xml',
         'security/ir.model.access.csv',
         'data/sequence_data.xml',
         'data/quality_team_data.xml',
