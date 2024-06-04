d67ad2a90b9184cff649bdb3b98bafd026102953
wendy <a@gmail.com>
2024-06-04 19:13:35 +0800

    V13.0 remove elw.quality.data contents  Changes to be committed: 	modified:   custom/elw_quality/models/quality_check.py 		add checks in do_measure, default_get 	modified:   custom/elw_quality/models/quality_measure_spec.py 		revise onchange functions 	modified:   custom/elw_quality/views/quality_check_view.xml 	modified:   custom/elw_quality/views/quality_control_point_view.xml 	modified:   custom/elw_quality/views/quality_measure_spec_view.xml

--- custom/elw_quality/models/quality_check.py
+++ custom/elw_quality/models/quality_check.py
@@ -54,17 +54,17 @@ class ElwQualityCheck(models.Model):
                                        compute="_compute_measured_spec", store=False)
     measure_data_count = fields.Integer("Measure Data Count", compute='_compute_measure_data_count')
     measure_data_ids = fields.One2many('elw.quality.measure.data', 'check_id', readonly=False,
-                                       compute="_compute_measured_data")
+                                       compute="", store=True)
 
-    # # measure_spec_ids must be filled if test_type_id == 5
-    # @api.constrains('measure_spec_ids')
-    # def _check_if_measure_spec_ids_empty(self):
-    #     for rec in self:
-    #         if rec.test_type_id.id == 5:
-    #             for each in rec.measure_spec_ids:
-    #                 if not each.measured_value:
-    #                     raise ValidationError(
-    #                         _("You have not updated Measured Value on %s in 'Measurement Data' tag", each.measure_name))
+    @api.depends('point_id')
+    def _check_measure_data_ids(self):
+        pass
+
+    @api.constrains('measure_data_ids')
+    def _check_if_measure_data_ids_empty(self):
+        for rec in self:
+            if rec.test_type_id.id == 5 and not len(rec.measure_data_ids):
+                raise ValidationError(_("Please fill in Measurement Data Tag."))
 
     @api.depends('test_type_id')
     def _compute_measured_data(self):
@@ -82,45 +82,18 @@ class ElwQualityCheck(models.Model):
             if rec.test_type_id.id == 5:
                 data = self.env['elw.quality.measure.spec'].search([('point_id', '=', rec.point_id.id)])
                 # data = self.env['elw.quality.measure.spec'].browse(rec.point_id.id)
-                # print()
+
                 rec.measure_spec_ids = data
+                print("rec.measure_spec_ids---", rec.measure_spec_ids)
             else:
                 rec.measure_spec_ids = None
 
     @api.depends('measure_spec_ids')
     def _compute_measure_data_count(self):
         for rec in self:
-            rec.measure_data_count = self.env['elw.quality.measure.spec'].search_count(
-                [('point_id', '=', rec.point_id.id)])
-
-    # update measured_value in elw.quality.measure.spec and save the records in measure.data
-    @api.depends('measure_spec_ids')
-    def action_measure_data_confirm(self):
-        for line in self.measure_spec_ids:
-            # print('line.id, line.name', line.id, line.name, self.id, line.check_id)
-            if not line.measured_value:
-                raise ValidationError(
-                    _("You have not updated Measured Value in Measure name: %s",
-                      line.measure_name))
-            else:
-                vals = {
-                    'measure_name': line.measure_name,
-                    'target_value': line.target_value,
-                    'measured_value': line.measured_value,
-                    'target_value_unit': line.target_value_unit,
-                    'upper_limit': line.upper_limit,
-                    'lower_limit': line.lower_limit,
-                    'within_tolerance': line.within_tolerance,
-                    'point_id': line.point_id.id,
-                    'check_id': self.id,
-                }
-                # print("vals----------", vals)
-                data_obj = self.env['elw.quality.measure.data']
-                create_id = data_obj.create(vals)
-                # print("create_id", create_id, create_id.id)
-                # reset
-                line.measured_value = 0
-                line.within_tolerance = False
+            rec.measure_data_count = self.env['elw.quality.measure.data'].search_count(
+                [('check_id', '=', rec.id)])
+            # print("rec.measure_data_count   : ", rec.measure_data_count)
 
     # if manually creating a qa check by selecting qa.point, make sure the product is in qa.point
     @api.constrains('product_id')
@@ -171,7 +144,7 @@ class ElwQualityCheck(models.Model):
         for vals in vals:
             vals['name'] = self.env['ir.sequence'].next_by_code(
                 'elw.quality.check.sequence')
-            print("before create vals", vals)
+            # print("before create vals", vals)
             rtn = super(ElwQualityCheck, self).create(vals)
         return rtn
 
@@ -209,39 +182,51 @@ class ElwQualityCheck(models.Model):
 
     def do_measure(self):
         qa_measure_state = []
-        if not self.measure_data_ids:
-            for line in self.measure_spec_ids:
-                # print('line.id, line.name', line.id, line.name, self.id, line.check_id)
-                if not line.measured_value:
+        measure_names =[]
+        if self.measure_data_ids:
+            for line in self.measure_data_ids:
+                if line.point_id.id != self.point_id.id:
+                    raise ValidationError(
+                        _("Selected Control Point Ref# %s does not match with the current: %s",
+                          line.point_id.name, self.point_id.name))
+                elif not line.measured_value:
                     raise ValidationError(
                         _("You have not updated Measured Value in Measure name: %s",
                           line.measure_name))
                 else:
-                    vals = {
-                        'measure_name': line.measure_name,
-                        'target_value': line.target_value,
-                        'measured_value': line.measured_value,
-                        'target_value_unit': line.target_value_unit,
-                        'upper_limit': line.upper_limit,
-                        'lower_limit': line.lower_limit,
-                        'within_tolerance': line.within_tolerance,
-                        'point_id': line.point_id.id,
-                        'check_id': self.id,
-                    }
-                    # print("vals----------", vals)
                     qa_measure_state.append(line.within_tolerance)
-                    data_obj = self.env['elw.quality.measure.data']
-                    create_id = data_obj.create(vals)
-                    # print("create_id", create_id, create_id.id)
-                    # reset
-                    line.measured_value = 0
-                    line.within_tolerance = False
-
-            # print("qa_measure_state", qa_measure_state)
-            if False in qa_measure_state:
-                self.quality_state = 'fail'
+                    measure_names.append(line.measure_name)
+            # print("measure_names", measure_names, qa_measure_state)
+            # check if the total measurement count id correct
+            cnt = len(self.env['elw.quality.measure.spec'].search([('point_id.id', '=', self.point_id.id)]))
+            if cnt != len(self.measure_data_ids):
+                raise ValidationError(
+                    _("Selected %d measurement items, not matching with the %d items in Quality Control Point",
+                      len(self.measure_data_ids), cnt))
+            # check if any measure_name duplication
+            elif len(measure_names) != len(set(measure_names)):
+                raise ValidationError(
+                    _("Duplicated Measure Name found !"))
             else:
-                self.quality_state = 'pass'
+                # print("qa_measure_state", qa_measure_state)
+                if False in qa_measure_state:
+                    self.quality_state = 'fail'
+                else:
+                    self.quality_state = 'pass'
+        else:
+            raise ValidationError(_("Please fill up the Measurement Data"))
+
+    # below can change the existing record's spec_id value, but cannot add records
+    @api.depends('point_id','measure_spec_ids')
+    def do_add_records(self):
+        self.ensure_one()
+        for item in self.measure_spec_ids:
+            print(item)
+            self.measure_data_ids = self.env['elw.quality.measure.data'].browse(1)
+            self.measure_data_ids.spec_id = item
+            print(self.measure_data_ids, self.measure_data_ids.spec_id)
+
+
 
     @api.model
     def _create_qa_alert_record(self, vals):
@@ -263,7 +248,6 @@ class ElwQualityCheck(models.Model):
         }
         # print("vals in quality.check---------", vals)
         qa_alert_rec = self._create_qa_alert_record(vals)
-
         return {
             'name': _('Quality Alert'),
             'res_model': 'elw.quality.alert',
@@ -322,3 +306,19 @@ class ElwQualityCheck(models.Model):
             'view_mode': 'tree,form',
             'target': 'current',
         }
+
+    @api.model
+    def default_get(self, fields_list):
+        res = super(ElwQualityCheck, self).default_get(fields_list)
+        # print("default_get---", fields_list) # all the fields
+        # print("res ---", res) # all default value
+        if not res.get('team_id'):
+            res['team_id'] = self.env['elw.quality.team'].browse(1).id
+            # res['test_type_id'] = 5
+        #     print("res 5 ---", res)
+        #     unsupported operand type(s) for "==": 'elw.quality.measure.data()' == '<Command.UPDATE: 1>'
+        #     res['measure_data_ids'] =self.env['elw.quality.measure.data'].browse(3)
+        # print("res ---", res)
+        return res
+
+
--- custom/elw_quality/models/quality_measure_spec.py
+++ custom/elw_quality/models/quality_measure_spec.py
@@ -12,25 +12,22 @@ class QualityMeasureSpec(models.Model):
     name = fields.Char('Sequence', readonly=True)
     measure_name = fields.Char('Measure Name', required=True, translate=True, tracking=True, store=True)
     target_value = fields.Float(string="Target Value")
-    measured_value = fields.Float(string="Measured Value", tracking=True, store=True)
+    # measured_value = fields.Float(string="Measured Value", tracking=True, store=True)
     target_value_unit = fields.Char(string="Unit", tracking=True, required=True, store=True)
     upper_limit = fields.Float(string="Upper Limit", tracking=True, required=True, store=True)
     lower_limit = fields.Float(string="Lower Limit", tracking=True, required=True, store=True)
-    within_tolerance = fields.Boolean('Within Tolerance?', default=False, tracking=True, store=True)
+    # within_tolerance = fields.Boolean('Within Tolerance?', default=False, tracking=True, store=True)
     point_id = fields.Many2one('elw.quality.point', 'Control Point Ref#', ondelete='cascade')
     product_id = fields.Many2one('product.product', string="Products", domain="[('type','in',('product','consu'))]",
-                                 store=True, related="point_id.product_id")
+                                 store=True, related="point_id.product_id", ondelete='cascade')
     date_created = fields.Date(string="Date Created", default=fields.Date.context_today)
-    check_id = fields.Many2one('elw.quality.check', string='Check Ref#',
+    check_id = fields.Many2one('elw.quality.check', string='Check Ref#', ondelete='cascade',
                                store=True)  # check_id is name of quality.check
 
-    @api.depends('point_id', 'check_id')
+    @api.depends('point_id', 'product_id')
     def _compute_display_name(self):
         for rec in self:
-            if rec.check_id.name:
-                rec.display_name = rec.name + ':' + rec.point_id.name + ':' + rec.check_id.name
-            else:
-                rec.display_name = rec.name + ':' + rec.point_id.name
+            rec.display_name = rec.point_id.name + ':' + rec.product_id.name
 
     @api.onchange('measured_value')
     def onchange_measured_value(self):
@@ -97,48 +94,36 @@ class QualityMeasureData(models.Model):
 
     active = fields.Boolean(default=True)
     name = fields.Char('Sequence', readonly=True)
-    # spec_id = fields.Many2one('elw.quality.measure.spec', string="Measure Spec Ref#")
-    measure_name = fields.Char('Measure Name', required=True, translate=True, tracking=True, store=True)
-    target_value = fields.Float(string="Target Value")
+    spec_id = fields.Many2one('elw.quality.measure.spec', string="Measure Spec Ref#", ondelete='cascade')
+    measure_name = fields.Char('Measure Name', related="spec_id.measure_name", translate=True, tracking=True, store=True)
+    target_value = fields.Float(string="Target Value", related="spec_id.target_value",)
     measured_value = fields.Float(string="Measured Value", tracking=True, store=True)
-    target_value_unit = fields.Char(string="Unit", required=True, tracking=True, store=True)
-    upper_limit = fields.Float(string="Upper Limit", required=True, tracking=True, store=True)
-    lower_limit = fields.Float(string="Lower Limit", required=True, tracking=True, store=True)
-    within_tolerance = fields.Boolean('Within Tolerance?', default=False, tracking=True, store=True)
-    point_id = fields.Many2one('elw.quality.point', 'Control Point Ref#', ondelete='cascade')
+    target_value_unit = fields.Char(string="Unit", related="spec_id.target_value_unit", tracking=True, store=True)
+    upper_limit = fields.Float(string="Upper Limit", related="spec_id.upper_limit", tracking=True, store=True)
+    lower_limit = fields.Float(string="Lower Limit", related="spec_id.lower_limit", tracking=True, store=True)
+    within_tolerance = fields.Boolean('Pass?', default=False, tracking=True, store=True)
+    point_id = fields.Many2one('elw.quality.point', 'QA Point Ref#', ondelete='cascade', related="spec_id.point_id")
     product_id = fields.Many2one('product.product', string="Products", domain="[('type','in',('product','consu'))]",
-                                 store=True, related="point_id.product_id")
+                                 store=True, related="spec_id.product_id", ondelete='cascade')
     date_created = fields.Date(string="Date Created", default=fields.Date.context_today)
-    check_id = fields.Many2one('elw.quality.check', string='Check Ref#',
+    check_id = fields.Many2one('elw.quality.check', string='Check Ref#', ondelete='cascade',
                                store=True)  # check_id is name of quality.check
 
-    # def _get_spec_id(self):
-    #     for line in self:
-    #         line.spec_id = self.env['elw.quality.measure.spec'].search([('point_id', '=', line.point_id)])
-
     # @api.onchange, it just not support one2many
-    @api.depends('measured_value', 'lower_limit', 'upper_limit')
-    def _recheck_measured_value(self):
-        for line in self.check_id.measure_data_ids:
-            if line.lower_limit <= line.measured_value <= line.upper_limit:
-                vals = {
-                    'within_tolerance': True,
-                }
-            else:
-                vals = {
-                    'within_tolerance': False,
-                }
-            line.update(vals)
-
-    # below checks both upper and lower limits
-    @api.depends('upper_limit', 'lower_limit')
-    def _recheck_upper_lower_limits(self):
-        for line in self.check_id.measure_data_ids:
-            # print(line.upper_limit, line.lower_limit)
-            if line.upper_limit < line.lower_limit:
-                raise ValidationError(_("The upper_limit must be > lower_limit"))
-                return -1
-        return 0
+    @api.onchange('measured_value')
+    def onchange_measured_value(self):
+        # print("measured_value", self.measured_value, self.upper_limit, self.lower_limit )
+        if self.measured_value and self.upper_limit and self.lower_limit:
+            for line in self.check_id.measure_data_ids:
+                if line.lower_limit <= line.measured_value <= line.upper_limit:
+                    vals = {
+                        'within_tolerance': True,
+                    }
+                else:
+                    vals = {
+                        'within_tolerance': False,
+                    }
+                line.update(vals)
 
     @api.model_create_multi
     def create(self, vals):
@@ -150,17 +135,10 @@ class QualityMeasureData(models.Model):
         return rtn
 
     # #  no decorator needed
-    # To Show tracking in chatter Add tracking=True https://www.odoo.com/forum/help-1/how-to-use-track-visibility-onchange-on-order-lines-fields-odoo14-211204
     def write(self, vals):
         if not self.name and not vals.get('name'):
             vals['name'] = self.env['ir.sequence'].next_by_code(
                 'elw.quality.measure.data.sequence')
         rtn = super(QualityMeasureData, self).write(vals)
-        # print("write  ............", vals)  # write  ............ {'measured_value': 1} one value
-        #  if condition is a must for avoiding the max recursion depth exceeded error
-        #  write 'measured_value' on all of one2many fields in the existing one record
-        if vals.get('measured_value') or vals.get('upper_limit') or vals.get('lower_limit'):
-            res = self._recheck_upper_lower_limits()
-            if res == 0:
-                self._recheck_measured_value()
         return rtn
+
