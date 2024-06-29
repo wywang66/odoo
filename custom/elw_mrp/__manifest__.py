e1370b666f6a3274f972ec7e43e19a42dcc964c1
wendy <a@gmail.com>
2024-06-29 12:06:00 +0800

    V21.0 inherit mrp add page 'Equipment' in workcenter form  Changes to be committed: 	new file:   custom/elw_mrp/__init__.py 	new file:   custom/elw_mrp/__manifest__.py 	new file:   custom/elw_mrp/controllers/__init__.py 	new file:   custom/elw_mrp/controllers/controllers.py 	new file:   custom/elw_mrp/demo/demo.xml 	new file:   custom/elw_mrp/models/__init__.py 	new file:   custom/elw_mrp/models/mrp.py 	new file:   custom/elw_mrp/security/ir.model.access.csv 	new file:   custom/elw_mrp/static/description/icon.png 	new file:   custom/elw_mrp/views/mrp_view.xml

---
+++ custom/elw_mrp/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "ELW Manufacturing",
+
+    'summary': "ELW Manufacturing",
+
+    'description': """
+Long description of module's purpose
+    """,
+    'application': True,
+    'sequence': -125,
+    'author': "Digital BigBite",
+    'website': "https://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'ELW/ELW Manufacturing',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'mrp', 'maintenance'],
+
+    # always loaded
+    'data': [
+        # 'security/elw_module_security_data.xml',
+        # 'security/ir.model.access.csv',
+        'views/mrp_view.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': True,
+}
