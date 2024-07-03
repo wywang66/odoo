7edfd2d45ea509d9cd784abb54c4df364f3986fc
taknetwendy <taknet_wendy@outlook.com>
2024-07-03 10:26:19 +0800

    V11.7 enable workcenter kanban/form view under maintenance|equipment  Changes to be committed: 	modified:   custom/elw_maintenance/views/maintenance_view_inherit.xml 	modified:   custom/elw_mrp/models/mrp.py 	modified:   custom/elw_mrp/views/mrp_view.xml

--- custom/elw_mrp/models/mrp.py
+++ custom/elw_mrp/models/mrp.py
@@ -5,6 +5,8 @@ class MaintenanceEquipment(models.Model):
     _inherit = 'maintenance.equipment'
 
     workcenter_id = fields.Many2one('mrp.workcenter', string='Work Center', ondelete='cascade')
+    # to avoid the validation error on equipment name
+    name = fields.Char('Equipment Name', required=False, translate=True)
 
 
 class MaintenanceMixin(models.AbstractModel):
@@ -14,13 +16,33 @@ class MaintenanceMixin(models.AbstractModel):
                                    help="Specific equipment that is used in this work center.", copy=True)
 
 
-class MrpWorkcenter(models.Model):
-    _inherit = ['mrp.workcenter',
-                # 'mail.thread',
-                # 'mail.activity.mixin',
-                ]  # add a chatter
+# # separate into two inheritance classes to avoid the "equipment_ids" not found error after inheriting 'mail' in MrpWorkcenter1
+# New ValueError: The _name attribute MrpWorkcenter is not valid.
+# class MrpWorkcenter(models.Model):
+#     _inherit = ['mail.thread',
+#                 'mail.activity.mixin',
+#                 ]  # add a chatter
+
+
+class MrpWorkcenter1(models.Model):
+    _inherit = 'mrp.workcenter'
+    # _inherit = ['mail.thread',
+    #             'mail.activity.mixin',
+    #             ]  # add a chatter
 
     equipment_ids = fields.One2many('maintenance.equipment', 'workcenter_id', string='Equipment', copy=True)
+    # TypeError: Many2many fields MrpWorkcenter.alternative_workcenter_ids and mrp.workcenter.alternative_workcenter_ids use the same table and columns
+    #  change relation to avoid the above error
+    # ValueError: The _name attribute MrpWorkcenter is not valid.
+    # alternative_workcenter_ids = fields.Many2many(
+    #     'mrp.workcenter',
+    #     'mrp_alternative_rel',
+    #     # 'workcenter_id',
+    #     # 'alternative_workcenter_id',
+    #     domain="[('id', '!=', id), '|', ('company_id', '=', company_id), ('company_id', '=', False)]",
+    #     string="Alternative Workcenters", check_company=True,
+    #     help="Alternative workcenters that can be substituted to this one in order to dispatch production"
+    # )
 
     company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company)
     maintenance_ids = fields.One2many('maintenance.request', 'workcenter_id', string='Maintenance', store=True)
