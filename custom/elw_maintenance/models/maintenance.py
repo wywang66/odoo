21a6969e57fb7c01b0366f9687cafcee2daf5715
wendy <a@gmail.com>
2024-07-07 15:47:21 +0800

    V21.7 use _get_workcenter_id for field workcenter_id  Changes to be committed: 	modified:   custom/elw_maintenance/models/maintenance.py 	modified:   custom/elw_maintenance/views/maintenance_view_inherit.xml

--- custom/elw_maintenance/models/maintenance.py
+++ custom/elw_maintenance/models/maintenance.py
@@ -12,4 +12,14 @@ class MaintenanceRequest(models.Model):
 class MaintenanceEquipment(models.Model):
     _inherit = 'maintenance.equipment'
 
-    workcenter_id = fields.Many2one('mrp.workcenter', string='Work Center', ondelete='cascade')
+    workcenter_id = fields.Many2one('mrp.workcenter', string='Work Center', ondelete='cascade', compute='_get_workcenter_id')
+
+    def _get_workcenter_id(self):
+        wc_obj_all = self.env['mrp.workcenter'].search([])
+        for rec in self:
+            rec.workcenter_id = False
+            for wc_obj in wc_obj_all:
+                if self in wc_obj.equipment_ids:
+                    rec.workcenter_id = wc_obj
+                    break
+
