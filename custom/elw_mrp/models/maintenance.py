6dba2423b4bcdd7293ee666a63883750d1b2e69c
wendy <a@gmail.com>
2024-07-08 20:26:36 +0800

    V21.10 move the inherit maintenance related method and view to elw_mrp update workcenter_id in maintenance.equipment using original id in onchange field  Changes to be committed: 	modified:   custom/elw_maintenance/__manifest__.py 	modified:   custom/elw_maintenance/models/__init__.py 	deleted:    custom/elw_maintenance/models/maintenance.py 	modified:   custom/elw_maintenance/views/maintenance_view_inherit.xml 	modified:   custom/elw_mrp/__manifest__.py 	modified:   custom/elw_mrp/models/__init__.py 	new file:   custom/elw_mrp/models/maintenance.py 	modified:   custom/elw_mrp/models/mrp.py 	new file:   custom/elw_mrp/views/maintenance_view_inherit.xml

---
+++ custom/elw_mrp/models/maintenance.py
@@ -0,0 +1,54 @@
+from odoo import api, fields, models, SUPERUSER_ID, _
+from odoo.exceptions import UserError, ValidationError
+
+
+class MaintenanceRequest(models.Model):
+    _inherit = 'maintenance.request'
+
+    maintenance_for = fields.Selection([('equipment', 'Equipment'), ('workcenter', 'Work Center')], string='For', store=True)
+    workcenter_id = fields.Many2one('mrp.workcenter', string='Work Center', ondelete='cascade')
+
+
+class MaintenanceEquipment(models.Model):
+    _inherit = 'maintenance.equipment'
+
+    workcenter_id = fields.Many2one('mrp.workcenter', string='Work Center', ondelete='cascade', compute='_get_workcenter_id', inverse='_inverse_workcenter_id', store=True)
+
+    @api.depends('workcenter_id.equipment_ids')
+    def _get_workcenter_id(self):
+        wc_obj_all = self.env['mrp.workcenter'].search([])
+        for rec in self:
+            rec.workcenter_id = False
+            for wc_obj in wc_obj_all:
+                if rec in wc_obj.equipment_ids:
+                    rec.workcenter_id = wc_obj
+                    break
+
+    def _inverse_workcenter_id(self):
+        for rec in self:
+            wc_obj_all = self.env['mrp.workcenter'].search([('equipment_ids', 'in', rec.id)])
+            # print('set: wc_obj_all', wc_obj_all)
+            if rec.workcenter_id:
+                # remove equipment from other work centers if there is
+                for wc_obj in wc_obj_all:
+                    if rec in wc_obj.equipment_ids and wc_obj != rec.workcenter_id:
+                        wc_obj.equipment_ids = [(3, rec.id)]  # remove
+                    # add equipment to the selected work center if not present
+                if rec not in rec.workcenter_id.equipment_ids:
+                    rec.workcenter_id.equipment_ids = [(4, rec.id)] # add
+                    # print('not in', rec.workcenter_id.equipment_ids, rec.id, rec.workcenter_id)
+            # else:
+            #     print("no wc")
+            #     for wc_obj in wc_obj_all:
+            #         if rec in wc_obj.equipment_ids:
+            #             wc_obj.equipment_ids = [(3, rec.id)]  # remove
+
+    def write(self, vals):
+        if 'workcenter_id' in vals:
+            print('in write', vals, vals['workcenter_id'], self._origin.id) # in write {'workcenter_id': 2} 2 5
+            equipment = self.env['maintenance.equipment'].browse(self._origin.id)
+            # print('equipment', equipment, self)  # maintenance.equipment(5,) maintenance.equipment(<NewId origin=5>,)
+            res = super(MaintenanceEquipment, equipment).write({'workcenter_id': vals['workcenter_id']})
+        else:
+            res = super(MaintenanceEquipment, self).write(vals)
+        return res
--- custom/elw_mrp/models/mrp.py
+++ custom/elw_mrp/models/mrp.py
@@ -1,28 +1,32 @@
+from typing import Dict, List
+
 from odoo import models, fields, api, SUPERUSER_ID, _
+import logging
+_logger = logging.getLogger(__name__)
 
-# # separate into two inheritance classes to avoid the "equipment_ids" not found error after inheriting 'mail' in MrpWorkcenter1
-# New ValueError: The _name attribute MrpWorkcenter is not valid.
-# class MrpWorkcenter(models.Model):
-#     _inherit = ['mail.thread',
-#                 'mail.activity.mixin',
-#                 ]  # add a chatter
-
-
-class MrpWorkcenter1(models.Model):
+class MrpWorkcenter(models.Model):
     _inherit = 'mrp.workcenter'
-    # _inherit = ['mail.thread',
-    #             'mail.activity.mixin',
-    #             ]  # add a chatter
+    # _inherit = ['mail.thread', 'mail.activity.mixin',]  # add a chatter
 
-    equipment_ids = fields.Many2many('maintenance.equipment', string='Equipment', copy=True)
-
+    # equipment_ids = fields.Many2many('maintenance.equipment', string='Equipment', store=True)
+    equipment_ids = fields.One2many('maintenance.equipment', 'workcenter_id', string='Equipment', store=True)
     company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company)
     maintenance_ids = fields.One2many('maintenance.request', 'workcenter_id', string='Maintenance', store=True)
     maintenance_count = fields.Integer(compute='_compute_maintenance_count', string="Maintenance Count", store=True)
     maintenance_open_count = fields.Integer(compute='_compute_maintenance_count', string="Current Maintenance", store=True)
     maintenance_team_id = fields.Many2one('maintenance.team', string='Maintenance Team', compute='_compute_maintenance_team_id', store=True, readonly=False,
                                           check_company=True)
-    technician_user_id = fields.Many2one('res.users', string='Technician', tracking=True)
+    technician_user_id = fields.Many2one('res.users', string='Technician')
+
+    # Update the workcenter_id in maintenance.equipment using the original object ID.
+    @api.onchange('equipment_ids')
+    def onchange_equipment_ids(self):
+        # print('equipment_ids', self.equipment_ids, self.equipment_ids.ids, self, self.id)  # maintenance.equipment(<NewId origin=8>, <NewId origin=13>)
+        # Onchange triggers with a new temporary object; the original object is accessible via self._origin.
+        # eq_obj is a dummy obj which cannot create a record. direct the call to 'write' method in maintenance.equipment to update the record
+        for eq_obj in self.equipment_ids:
+            eq_obj.write({'workcenter_id': self._origin.id})
+            # print('eq_obj info1: ', eq_obj, eq_obj.id, eq_obj.workcenter_id)  # maintenance.equipment(<NewId origin=3>,) NewId_3 mrp.workcenter()
 
     @api.depends('company_id')
     def _compute_maintenance_team_id(self):
