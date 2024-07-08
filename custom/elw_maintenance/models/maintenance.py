6434599d3e349bab8debdaaca67a57391749a2fa
taknetwendy <taknet_wendy@outlook.com>
2024-07-08 08:55:53 +0800

    V11.13 get workcenter_id by compute and inverse method remove equipment from original work center when changing workcenter  Changes to be committed: 	modified:   custom/elw_maintenance/__manifest__.py 	modified:   custom/elw_maintenance/models/maintenance.py 	modified:   custom/elw_maintenance/views/maintenance_view_inherit.xml

--- custom/elw_maintenance/models/maintenance.py
+++ custom/elw_maintenance/models/maintenance.py
@@ -7,3 +7,40 @@ class MaintenanceRequest(models.Model):
 
     maintenance_for = fields.Selection([('equipment', 'Equipment'), ('workcenter', 'Work Center')], string='For', store=True)
     workcenter_id = fields.Many2one('mrp.workcenter', string='Work Center', ondelete='cascade')
+
+
+class MaintenanceEquipment(models.Model):
+    _inherit = 'maintenance.equipment'
+
+    workcenter_id = fields.Many2one('mrp.workcenter', string='Work Center', ondelete='cascade', compute='_get_workcenter_id', inverse='_inverse_workcenter_id', store=True)
+
+    @api.depends('workcenter_id.equipment_ids')
+    def _get_workcenter_id(self):
+        wc_obj_all = self.env['mrp.workcenter'].search([])
+        for rec in self:
+            rec.workcenter_id = False
+            for wc_obj in wc_obj_all:
+                if rec in wc_obj.equipment_ids:
+                    rec.workcenter_id = wc_obj
+                    break
+
+    def _inverse_workcenter_id(self):
+        for rec in self:
+            wc_obj_all = self.env['mrp.workcenter'].search([('equipment_ids', 'in', rec.id)])
+            # print('wc_obj_all', wc_obj_all)
+            if rec.workcenter_id:
+                # remove equipment from other work centers if there is
+                for wc_obj in wc_obj_all:
+                    if rec in wc_obj.equipment_ids and wc_obj != rec.workcenter_id:
+                        wc_obj.equipment_ids = [(3, rec.id)]  # remove
+                    # add equipment to the selected work center if not present
+                if rec not in rec.workcenter_id.equipment_ids:
+                    rec.workcenter_id.equipment_ids = [(4, rec.id)] # add
+                    # print('not in', rec.workcenter_id.equipment_ids, rec.id, rec.workcenter_id)
+            else:
+                print("no wc")
+                for wc_obj in wc_obj_all:
+                    if rec in wc_obj.equipment_ids:
+                        wc_obj.equipment_ids = [(3, rec.id)]  # remove
+
+
