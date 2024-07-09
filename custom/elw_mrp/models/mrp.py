e23ef21a7b6518608e3d74107740afa1d9adad43
taknetwendy <taknet_wendy@outlook.com>
2024-07-09 10:05:17 +0800

    V11.18 add a chatter in workcenter  Changes to be committed: 	modified:   custom/elw_mrp/models/mrp.py 	modified:   custom/elw_mrp/views/mrp_view.xml

--- custom/elw_mrp/models/mrp.py
+++ custom/elw_mrp/models/mrp.py
@@ -5,8 +5,8 @@ import logging
 _logger = logging.getLogger(__name__)
 
 class MrpWorkcenter(models.Model):
-    _inherit = 'mrp.workcenter'
-    # _inherit = ['mail.thread', 'mail.activity.mixin',]  # add a chatter
+    _name = 'mrp.workcenter'
+    _inherit = ['mrp.workcenter', 'mail.thread', 'mail.activity.mixin']
 
     equipment_ids = fields.One2many('maintenance.equipment', 'workcenter_id', string='Equipment', store=True)
     company_id = fields.Many2one('res.company', string='Company', default=lambda self: self.env.company)
